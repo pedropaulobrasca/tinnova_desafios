d1c7874a7d0ab31e91aaf8af424270f770abdb86
Pedro Brasca <pedro.paulo@b2mlportal.com.br>
2022-05-07 23:38:54 -0300

    Front configurado com tailwindcss

---
+++ quinto_desafio_front/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          300: "#996DFF",
+          500: "#8257e6",
+        },
+      },
+      borderRadius: {
+        md: "0.25rem",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
+};
+
